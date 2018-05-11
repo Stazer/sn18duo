0ce92ed8ed09ef3f2d89699d4bcb9edad231f40a
Justus Flerlage <justusflerlage@juteel.com>
2018-05-11 17:09:01 +0200

    asldk

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/todos', to: 'todos#index'
-  resources :todos
+  root 'todos#index'
+  resources :todos
 end
