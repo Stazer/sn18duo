54bb86735952a571edbf84a19b905f5dea4cb635
Lukas Schmidt <lukas-schmidt@outlook.com>
2018-05-11 16:21:12 +0200

    itz itz

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,22 @@
+class TodosContoller < ApplicationController
+
+    def new
+    end
+    
+    def create
+    end
+
+    def index
+        @todos = Todo.all
+    end
+
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+
+end
